38d60b5011b481ac9e4e3ea48e9d24571d123804
imhsart <nitinrana2519@gmail.com>
2026-06-03 16:24:26 +0530

    Assignment attempt 2

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -3,7 +3,7 @@ import React from "react";
 const Tabs = ({tab}) => {
   return (
     <div className="tabs">
-      <div className="tab" id={`tabname-${tab}`}>
+      <div className="tab" id={`${tab}-Tab`}>
         {tab}
       </div>
     </div>
